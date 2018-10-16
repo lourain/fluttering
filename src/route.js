f5a93ba35afec0f844547c4323ba082f91242be4
lyw <491027175@qq.com>
2018-10-16 17:14:04 +0800

    搭建

---
+++ src/route.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import {Route} from 'react-router-dom'
+import App from './App'
+
+export default ()=>{
+    return [
+        <Route path="/123" component={App} />
+
+    ]
+}
