09f6dfa40a4df5b9a24c4b2b3c16d7c5dba1c6e6
lyw <491027175@qq.com>
2018-10-17 17:24:38 +0800

    qweqe

--- src/page/home/Home.js
+++ src/page/home/Home.js
@@ -1,13 +1,34 @@
 import React, { Component } from 'react';
 import styles from './Home.less'
-import chuan from '../../img/chuan.png'
+import autumn from '../../img/antumn.webp'
 class App extends Component {
   render() {
     return (
       <div className={styles.home}>
-        
         <header>风满楼</header>
-        <img src={chuan} alt=""/>
+        {/* <img src={chuan} alt=""/> */}
+        <div className={styles.content}>
+          <ul>
+            <li>
+              <a href="/detail">
+                <img src={autumn} alt="" />
+                <p>深秋于苏</p>
+              </a>
+            </li>
+            <li>
+              <a href="/detail">
+                <img src={autumn} alt="" />
+                <p>深秋于苏</p>
+              </a>
+            </li>
+            <li>
+              <a href="/detail">
+                <img src={autumn} alt="" />
+                <p>深秋于苏</p>
+              </a>
+            </li>
+          </ul>
+        </div>
       </div>
     )
   }
