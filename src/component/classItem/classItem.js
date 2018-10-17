558c7ecb96b373c78b565d2ef2b5fe0b54c8e4fe
lyw <491027175@qq.com>
2018-10-18 00:46:08 +0800

    动画

--- src/component/classItem/classItem.js
+++ src/component/classItem/classItem.js
@@ -3,20 +3,27 @@ import autumn from '../../img/antumn.webp'
 import styles from './classItem.less'
 
 class classItem extends Component {
-    constructor(){
+    constructor() {
         super()
         this.state = {
-            title:''
+            title: '',
+            fold: true,//是否收叠
         }
     }
+    toggle() {
+        this.setState({
+            fold: !this.state.fold
+        })
+        console.log(this.state.fold);
 
+    }
     render() {
-        const {itemName} = this.props
-        
+        const { itemName } = this.props
+        const { fold } = this.state
         return (
             <div>
-                <div className={styles.title}>{itemName}</div>
-                <ul>
+                <div className={styles.title} onClick={this.toggle.bind(this)}>{itemName}</div>
+                <ul className={!fold?styles.fold:styles.unfold}>
                     <li>
                         <a href="/detail">
                             <img src={autumn} alt="" />
@@ -48,6 +55,44 @@ class classItem extends Component {
                         </a>
                     </li>
                 </ul>
+                {/* {
+                    !fold ?
+                        <ul>
+                            <li>
+                                <a href="/detail">
+                                    <img src={autumn} alt="" />
+                                    <p>深秋于苏</p>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="/detail">
+                                    <img src={autumn} alt="" />
+                                    <p>深秋于苏深秋于苏深秋于苏</p>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="/detail">
+                                    <img src={autumn} alt="" />
+                                    <p>深秋于苏</p>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="/detail">
+                                    <img src={autumn} alt="" />
+                                    <p>深秋于苏</p>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="/detail">
+                                    <img src={autumn} alt="" />
+                                    <p>深秋于苏深秋于苏深秋于苏</p>
+                                </a>
+                            </li>
+                        </ul>
+                        :
+                        null
+                } */}
+
             </div>
         )
     }
