639538556706f249efb2ad8fae2cba2c9249566c
liyiwei <491027175@qq.com>
2018-10-17 16:15:12 +0800

    chuan

--- src/page/home/Home.js
+++ src/page/home/Home.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
 import styles from './Home.less'
+import chuan from '../../img/chuan.png'
 class App extends Component {
   render() {
     return (
       <div className={styles.home}>
         
         <header>风满楼</header>
-        
+        <img src={chuan} alt=""/>
       </div>
     )
   }
