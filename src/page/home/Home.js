2056177c95fe997d6d5963e10088ef49e66d1f0e
lyw <491027175@qq.com>
2018-10-17 18:42:10 +0800

    fsfs

--- src/page/home/Home.js
+++ src/page/home/Home.js
@@ -2,32 +2,38 @@ import React, { Component } from 'react';
 import styles from './Home.less'
 import autumn from '../../img/antumn.webp'
 class App extends Component {
+  // constructor(){
+    
+  // }
   render() {
     return (
       <div className={styles.home}>
         <header>风满楼</header>
-        {/* <img src={chuan} alt=""/> */}
         <div className={styles.content}>
-          <ul>
-            <li>
-              <a href="/detail">
-                <img src={autumn} alt="" />
-                <p>深秋于苏</p>
-              </a>
-            </li>
-            <li>
-              <a href="/detail">
-                <img src={autumn} alt="" />
-                <p>深秋于苏</p>
-              </a>
-            </li>
-            <li>
-              <a href="/detail">
-                <img src={autumn} alt="" />
-                <p>深秋于苏</p>
-              </a>
-            </li>
-          </ul>
+          <div className={styles.daily}>
+            <div className={styles.title}>游记</div>
+            <ul>
+              <li>
+                <a href="/detail">
+                  <img src={autumn} alt="" />
+                  <p>深秋于苏</p>
+                </a>
+              </li>
+              <li>
+                <a href="/detail">
+                  <img src={autumn} alt="" />
+                  <p>深秋于苏深秋于苏深秋于苏</p>
+                </a>
+              </li>
+              <li>
+                <a href="/detail">
+                  <img src={autumn} alt="" />
+                  <p>深秋于苏</p>
+                </a>
+              </li>
+            </ul>
+
+          </div>
         </div>
       </div>
     )
