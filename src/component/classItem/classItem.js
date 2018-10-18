4102618328e53ea956e1856c9e387f9dbcdf7144
liyiwei <491027175@qq.com>
2018-10-18 17:11:35 +0800

    手风琴效果

--- src/component/classItem/classItem.js
+++ src/component/classItem/classItem.js
@@ -7,53 +7,34 @@ class classItem extends Component {
         super()
         this.state = {
             fold: true,//是否收叠
-            
+
         }
     }
     toggle() {
         this.setState({
             fold: !this.state.fold
         })
-        // this.props.broadcastFold(true)
     }
     render() {
-        const { itemName,broadcastFold } = this.props
+        const { itemData, broadcastFold } = this.props
         const { fold } = this.state
-        
+        console.log(fold)
         return (
             <div>
-                <div className={styles.title} onClick={this.toggle.bind(this)}>{itemName}</div>
-                <ul className={fold&&broadcastFold?styles.fold:styles.unfold}>
-                    <li>
-                        <a href="/detail">
-                            <img src={autumn} alt="" />
-                            <p>深秋于苏</p>
-                        </a>
-                    </li>
-                    <li>
-                        <a href="/detail">
-                            <img src={autumn} alt="" />
-                            <p>深秋于苏深秋于苏深秋于苏</p>
-                        </a>
-                    </li>
-                    <li>
-                        <a href="/detail">
-                            <img src={autumn} alt="" />
-                            <p>深秋于苏</p>
-                        </a>
-                    </li>
-                    <li>
-                        <a href="/detail">
-                            <img src={autumn} alt="" />
-                            <p>深秋于苏</p>
-                        </a>
-                    </li>
-                    <li>
-                        <a href="/detail">
-                            <img src={autumn} alt="" />
-                            <p>深秋于苏深秋于苏深秋于苏</p>
-                        </a>
-                    </li>
+                <div className={styles.title} onClick={this.toggle.bind(this)}>{itemData.item_name}</div>
+                <ul className={fold  ? styles.fold : styles.unfold}>
+                    {
+                        itemData.list.map((article,index) => {
+                            return (
+                                <li key={index}>
+                                    <a href="/detail">
+                                        <img src={autumn} alt="" />
+                                        <p>{article.article_title}</p>
+                                    </a>
+                                </li>
+                            )
+                        })
+                    }
                 </ul>
             </div>
         )
--- src/page/home/Home.js
+++ src/page/home/Home.js
@@ -1,21 +1,94 @@
 import React, { Component } from 'react';
 import styles from './Home.less'
-import ClassItem from '../../component/classItem/classItem'
+import autumn from '../../img/antumn.webp'
+
 class App extends Component {
     constructor() {
         super()
         this.state = {
-            broadcastFold:true
+            broadcastFold: true,
+            data: [
+                {
+                    item_name: '游记',
+                    list: [
+                        { article_title: '深秋于苏' },
+                        { article_title: '深秋于苏深秋于苏深秋于苏' },
+                        { article_title: '深秋于苏' },
+                    ]
+                },
+                {
+                    item_name: '心情',
+                    list: [
+                        { article_title: '深秋于苏' },
+                        { article_title: '深秋于苏深秋于苏深秋于苏' },
+                        { article_title: '深秋于苏' },
+                    ]
+                },
+                {
+                    item_name: '图片',
+                    list: [
+                        { article_title: '深秋于苏' },
+                        { article_title: '深秋于苏深秋于苏深秋于苏' },
+                        { article_title: '深秋于苏' },
+                    ]
+                }
+
+            ]
         }
+        this.state.data.forEach(v => {
+            v.fold = true
+        })
+    }
+    toggle(index) {
+        let new_data = this.state.data
+        new_data.forEach((value, i) => {
+            if (i === index) {
+                value.fold = !value.fold
+            } else {
+                value.fold = true
+
+            }
+        })
+        this.setState({
+            data: new_data
+        })
+        console.log(new_data[index].fold)
+
     }
     render() {
-        const {broadcastFold} = this.state
+        const { data } = this.state
+
         return (
             <div className={styles.home}>
                 <header>风满楼</header>
                 <div className={styles.content}>
-                    <ClassItem itemName={'游记'} broadcastFold={broadcastFold}/>
-                    <ClassItem itemName={'心情'} broadcastFold={broadcastFold}/>
+                    {
+                        data.map((item, index) => {
+                            return (
+                                <div key={index}>
+                                    <div className={styles.title} onClick={this.toggle.bind(this, index)}>{item.item_name}</div>
+                                    <ul className={item.fold ? styles.fold : styles.unfold}>
+                                        {
+                                            item.list.map((article, index) => {
+                                                return (
+                                                    <li key={index}>
+                                                        <a href="/detail">
+                                                            <img src={autumn} alt="" />
+                                                            <p>{article.article_title}</p>
+                                                        </a>
+                                                    </li>
+
+                                                )
+                                            })
+                                        }
+
+                                    </ul>
+                                </div>
+
+                            )
+                        })
+                    }
+
                 </div>
             </div>
         )
