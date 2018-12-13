2b90b0cee5a505c3b5663dbc185950a3ebd5df62
liyiwei <491027175@qq.com>
2018-12-13 15:40:06 +0800

    editor

---
+++ src/page/article/article.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+import style from './article.less'
+
+export default class Article extends Component {
+	constructor() {
+		super()
+	}
+
+	render() {
+		return (
+			<div className={style.article}>
+                                
+			</div>
+		)
+	}
+}
--- src/page/home/Home.js
+++ src/page/home/Home.js
@@ -4,112 +4,84 @@ import autumn from '../../img/antumn.webp'
 import Lotus from '../../img/logo.png'
 import request from '../../request'
 class App extends Component {
-    constructor() {
-        super()
-        this.state = {
-            // data: [
-            //     {
-            //         item_name: '游记',
-            //         list: [
-            //             { title: '深秋于苏' },
-            //             { title: '深秋于苏深秋于苏深秋于苏' },
-            //             { title: '深秋于苏' },
-            //         ]
-            //     },
-            //     {
-            //         item_name: '心情',
-            //         list: [
-            //             { title: '深秋于苏' },
-            //             { title: '深秋于苏深秋于苏深秋于苏' },
-            //             { title: '深秋于苏' },
-            //         ]
-            //     },
-            //     {
-            //         item_name: '图片',
-            //         list: [
-            //             { title: '深秋于苏' },
-            //             { title: '深秋于苏深秋于苏深秋于苏' },
-            //             { title: '深秋于苏' },
-            //         ]
-            //     }
+	constructor() {
+		super()
+		this.state = {
+			data: []
+		}
 
-            // ]
-            data:[]
-        }
-        
-    }
-    componentWillMount(){
-        this.getData()
-        
-    }
-    getData() {
-        request('get','/directory')
-            .then(res=>{
-                let _data = res.data
-                _data.forEach(v=>{
-                    v.fold = true
-                })
-                this.setState({
-                    data:_data
-                })
-                
-            })
-    }
-    toggle(index) {
-        let new_data = this.state.data
-        new_data.forEach((value, i) => {
-            if (i === index) {
-                value.fold = !value.fold
-            } else {
-                value.fold = true
-            }
-        })
-        this.setState({
-            data: new_data
-        })
-        new_data = null
-    }
-    render() {
-        const { data } = this.state
-        
-        return (
-            <div className={styles.home}>
-                <header>
-                    <p>风满楼</p>
-                    {/* <img src={Lotus} alt="" /> */}
-                    <div className={styles.logo}></div>
-                </header>
-                <div className={styles.content}>
-                    {
-                        data.map((item, index) => {
-                            return (
-                                <div key={index}>
-                                    <div className={styles.title} onClick={this.toggle.bind(this, index)}>{item.item_name}</div>
-                                    <ul className={item.fold ? styles.fold : styles.unfold}>
-                                        {
-                                            item.list.map((article, index) => {
-                                                return (
-                                                    <li key={index}>
-                                                        <a href="/detail">
-                                                            <img src={autumn} alt="" />
-                                                            {/* <div className="img" styles={{backgroundImage:'require('+autumn+')'}}></div> */}
-                                                            <p>{article.title||article.album_name}</p>
-                                                        </a>
-                                                    </li>
-                                                )
-                                            })
-                                        }
-                                    </ul>
-                                </div>
+	}
+	componentWillMount() {
+		this.getData()
 
-                            )
-                        })
-                    }
+	}
+	getData() {
+		request('get', '/directory')
+			.then(res => {
+				let _data = res.data
+				_data.forEach(v => {
+					v.fold = true
+				})
+				this.setState({
+					data: _data
+				})
 
-                </div>
-            </div >
-        )
-    }
+			})
+	}
+	toggle(index) {
+		let new_data = this.state.data
+		new_data.forEach((value, i) => {
+			if (i === index) {
+				value.fold = !value.fold
+			} else {
+				value.fold = true
+			}
+		})
+		this.setState({
+			data: new_data
+		})
+		new_data = null
+	}
+	render() {
+		const { data } = this.state
+
+		return (
+			<div className={styles.home}>
+				<header>
+					<p>风满楼</p>
+					<div className={styles.logo}></div>
+				</header>
+				<div className={styles.content}>
+					{
+						
+						data.map((item, index) => {
+							return (
+								<div key={index}>
+									<div className={styles.title} onClick={this.toggle.bind(this, index)}>{item.item_name}</div>
+									<ul className={item.fold ? styles.fold : styles.unfold}>
+										{
+											item.list.map((article, index) => {
+												return (
+													<li key={index}>
+														<a href="/detail">
+															<img src={autumn} alt="" />
+															<p>{article.title || article.album_name}</p>
+														</a>
+													</li>
+												)
+											})
+										}
+									</ul>
+								</div>
+
+							)
+						})
+					}
+
+				</div>
+			</div >
+		)
+	}
 }
 
 export default App;
