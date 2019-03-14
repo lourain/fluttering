c7dab3366e6cdb855d121881380b3b4680e79a89
lyw <491027175@qq.com>
2019-03-14 20:54:10 +0800

    forkmode

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -5,7 +5,7 @@ module.exports = {
 
     // Options reference: https://pm2.io/doc/en/runtime/reference/ecosystem-file/
     args: 'run server',
-    instances: 1,
+    // instances: 1,
     autorestart: true,
     watch: true,
     max_memory_restart: '1G',
