212f709844b6301cb00d62918fc1f8027dbfbf13
lyw <491027175@qq.com>
2019-04-05 09:01:14 +0800

    fix preload

--- server/server.js
+++ server/server.js
@@ -17,7 +17,8 @@ import marked from 'marked'
 
 assetHook({
     extensions: ['jpg', 'png', 'webp', 'ttf'],
-    name: 'static/media/[name].[hash:8].[ext]'
+    // name: 'static/media/[name].[hash:8].[ext]'
+    name: 'static/media/[name].[ext]'
 })
 
 const app = express()
@@ -84,12 +85,12 @@ app.get('*', async (req, res, next) => {
             <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
             <meta http-equiv="X-UA-Compatible" content="ie=edge"/>
             <title>風滿樓</title>
-            <link rel="preload shortcut icon" href="/favicon.ico"/>
+            <link rel="preload shortcut icon" as="icon" href="/favicon.ico"/>
             <link rel="preload stylesheet" as="style" href="${manifestPath['main.css']}"/>
             <link rel="preload stylesheet" as="style" href="${manifestPath["static/css/1.81851846.chunk.css"]}"/>
             <link rel="preload" as="font" href="https://www.fluttering.cn/uploads/lixuke.css">
-            <link rel="preload"  href="${manifestPath['static/media/Home.less']}">
-            <link rel="preload"  href="${manifestPath['static/media/lotus.png']}">
+            <link rel="preload" as="image" href="${manifestPath['static/media/Home.less']}">
+            <link rel="preload" as="image" href="${manifestPath['static/media/lotus.png']}">
             </head>
         <body>
             <div id="root">
@@ -107,8 +108,8 @@ app.get('*', async (req, res, next) => {
                 </div>
                 <script>window.__initData__ = ${JSON.stringify(preloadedState)}</script>
                 <script>!function (l) { function e(e) { for (var r, t, n = e[0], o = e[1], u = e[2], f = 0, i = []; f < n.length; f++)t = n[f], p[t] && i.push(p[t][0]), p[t] = 0; for (r in o) Object.prototype.hasOwnProperty.call(o, r) && (l[r] = o[r]); for (s && s(e); i.length;)i.shift()(); return c.push.apply(c, u || []), a() } function a() { for (var e, r = 0; r < c.length; r++) { for (var t = c[r], n = !0, o = 1; o < t.length; o++) { var u = t[o]; 0 !== p[u] && (n = !1) } n && (c.splice(r--, 1), e = f(f.s = t[0])) } return e } var t = {}, p = { 2: 0 }, c = []; function f(e) { if (t[e]) return t[e].exports; var r = t[e] = { i: e, l: !1, exports: {} }; return l[e].call(r.exports, r, r.exports, f), r.l = !0, r.exports } f.m = l, f.c = t, f.d = function (e, r, t) { f.o(e, r) || Object.defineProperty(e, r, { enumerable: !0, get: t }) }, f.r = function (e) { "undefined" != typeof Symbol && Symbol.toStringTag && Object.defineProperty(e, Symbol.toStringTag, { value: "Module" }), Object.defineProperty(e, "__esModule", { value: !0 }) }, f.t = function (r, e) { if (1 & e && (r = f(r)), 8 & e) return r; if (4 & e && "object" == typeof r && r && r.__esModule) return r; var t = Object.create(null); if (f.r(t), Object.defineProperty(t, "default", { enumerable: !0, value: r }), 2 & e && "string" != typeof r) for (var n in r) f.d(t, n, function (e) { return r[e] }.bind(null, n)); return t }, f.n = function (e) { var r = e && e.__esModule ? function () { return e.default } : function () { return e }; return f.d(r, "a", r), r }, f.o = function (e, r) { return Object.prototype.hasOwnProperty.call(e, r) }, f.p = "./"; var r = window.webpackJsonp = window.webpackJsonp || [], n = r.push.bind(r); r.push = e, r = r.slice(); for (var o = 0; o < r.length; o++)e(r[o]); var s = n; a() }([])</script>                    
-                <link rel="preload" href=${manifestPath["static/js/1.37e8ee76.chunk.js"]}/>
-                <link rel="preload" href=${manifestPath["main.js"]}/>
+                <link rel="preload" as="script" href=${manifestPath["static/js/1.37e8ee76.chunk.js"]}/>
+                <link rel="preload" as="script" href=${manifestPath["main.js"]}/>
             </body>
         </html>`)
         res.end()
